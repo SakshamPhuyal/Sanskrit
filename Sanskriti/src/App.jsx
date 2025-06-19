afa1987add19fa9fae8002df4461749f345d2971
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-06-19 23:09:00 +0545

    tailwind

--- Sanskriti/src/App.jsx
+++ Sanskriti/src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import {Routes,Route} from 'react-router-dom'
 import './App.css'
+import Nav from './Components/nav/nav';
 import Home from './Components/Pages/Home';
 import Collection from './Components/Pages/Collection';
 import About from './Components/Pages/About';
 import Contact from './Components/Pages/Contact';
-import product from './Components/Pages/product';
-import cart from './Components/Pages/cart';
+import Product from './Components/Pages/product';
+import Cart from './Components/Pages/cart';
 import Login from './Components/Pages/Login';
 import Orders from './Components/Pages/Orders';
-import shop from './Components/Pages/shop';
-import productdetails from './Components/Pages/productdetails';
+import Shop from './Components/Pages/shop';
+import Productdetails from './Components/Pages/productdetails';
 function App() {
 
 
@@ -23,12 +24,12 @@ function App() {
 <Route path='/collection' element={<Collection/>} /> 
 <Route path='/about' element={<About/>} />
 <Route path='/contact' element={<Contact/>} />  
-<Route path='/product/:productId' element={<product/>} /> 
-<Route path='/cart' element={<cart/>} /> 
+<Route path='/product/:productId' element={<Product/>} /> 
+<Route path='/cart' element={<Cart/>} /> 
 <Route path='/login' element={<Login/>} /> 
 <Route path='/orders' element={<Orders/>} /> 
-<Route path='/shop' element={<shop/>} /> 
-<Route path='/productdetails' element={<productdetails/>} /> 
+<Route path='/shop' element={<Shop/>} /> 
+<Route path='/productdetails' element={<Productdetails/>} /> 
     </Routes>
   </div>
     </>
--- Sanskriti/src/Components/nav/nav.jsx
+++ Sanskriti/src/Components/nav/nav.jsx
@@ -1,12 +1,18 @@
- import React from 'react'
- 
- const nav = () => {
-   return (
-     <div className='navbar'>
-       
-     </div>
-   )
- }
- 
- export default nav
- className='navbar' 
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav className="bg-orange-100 shadow-md p-4 flex h-16 justify-between px-20">
+      <div className='flex gap-1 text-2xl'><p>Sanskriti Bazar</p><img className='h-11.5 w-11.5' src="https://imgs.search.brave.com/7OKwT_y5F8aVe2VImisGFjaW_ThkDFcKGpodUJk0jec/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/ZHJpYmJibGUuY29t/L3VzZXJzLzEyNDQ0/Ny9zY3JlZW5zaG90/cy8xNzkyMzU2L3Nh/bnNrcml0aS5qcGc_/cmVzaXplPTQwMHgz/MDAmdmVydGljYWw9/Y2VudGVy" alt="" /></div>
+   <ul className='cursor-pointer text-2xl flex gap-x-19'>
+    <li>Home</li>
+    <li>Shop</li>
+    <li>Cart</li>
+    <li>About</li>
+    <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
