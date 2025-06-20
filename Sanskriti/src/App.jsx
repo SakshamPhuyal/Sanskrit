d6f92e4f3dc112b76c1658540bfa55f44dfeb5a5
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-06-20 18:50:02 +0545

    Routing

--- Sanskriti/src/App.jsx
+++ Sanskriti/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Routes,Route} from 'react-router-dom'
 import './App.css'
+import './index.css'
 import Nav from './Components/nav/nav';
 import Home from './Components/Pages/Home';
-import Collection from './Components/Pages/Collection';
+import Explore from './Components/Pages/Explore';
 import About from './Components/Pages/About';
 import Contact from './Components/Pages/Contact';
 import Product from './Components/Pages/product';
@@ -21,7 +22,7 @@ function App() {
     <Nav />
     <Routes>
 <Route path='/' element={<Home/>} /> 
-<Route path='/collection' element={<Collection/>} /> 
+<Route path='/explore' element={<Explore/>} /> 
 <Route path='/about' element={<About/>} />
 <Route path='/contact' element={<Contact/>} />  
 <Route path='/product/:productId' element={<Product/>} /> 
--- Sanskriti/src/Components/nav/nav.jsx
+++ Sanskriti/src/Components/nav/nav.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-
+import {NavLink} from 'react-router-dom';
 const Nav = () => {
   return (
-<nav className="bg-[#5a3e36]/70 text-white shadow-md p-4 flex h-16 justify-between px-20 fixed top-0 w-full z-50">
+<nav className="bg-[#5a3e36]/70 text-white shadow-md p-4 flex h-16 justify-between px-20 fixed top-0 w-full z-50 ">
       <div className='flex gap-3 text-3xl'><p>Sanskriti Bazar</p><img className='h-9 w-9 rounded-lg' src="https://imgs.search.brave.com/7OKwT_y5F8aVe2VImisGFjaW_ThkDFcKGpodUJk0jec/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/ZHJpYmJibGUuY29t/L3VzZXJzLzEyNDQ0/Ny9zY3JlZW5zaG90/cy8xNzkyMzU2L3Nh/bnNrcml0aS5qcGc_/cmVzaXplPTQwMHgz/MDAmdmVydGljYWw9/Y2VudGVy" alt="" /></div>
-   <ul className='cursor-pointer text-2xl flex gap-x-19'>
-    <li to='/'>Home  <hr className='w-4/4 border-none h-1.5 bg-gray-700' /></li>
-       <li to='/shop'>Shop <hr className='w-4/4 border-none h-1.5 bg-gray-700' /></li>
-       <li to='/collection'>Cart <hr className='w-4/4 border-none h-1.5 bg-gray-700' /></li>
-       <li to='/about'>About <hr className='w-4/4 border-none h-1.5 bg-gray-700' /></li>
-        <li to='/contact'>Contact <hr className='w-4/4 border-none h-1.5 bg-gray-700' /></li>
-      </ul>
+   <div className='cursor-pointer text-2xl flex gap-x-19'>
+    <div><NavLink to='/'>Home  </NavLink>
+    <hr className='w-4/4 border-none h-1 bg-orange-300 hidden' />
+    </div>
+       <div><NavLink to='/shop'>Shop</NavLink>
+<hr className='w-4/4 border-none h-1 bg-orange-300 hidden' />
+</div>
+<div>
+       <NavLink to='/explore'>Explore </NavLink>
+       <hr className='block w-4/4 border-0 h-1 bg-orange-300 hidden' />
+       </div>
+       <div >
+       <NavLink to='/about'>About</NavLink>
+       <hr className='w-4/4 border-none h-1 bg-orange-300 hidden' />
+       </div>
+       <div>
+        <NavLink to='/contact'>Contact</NavLink>
+        <hr className='w-4/4 border-none h-1 bg-orange-300 hidden' />
+        </div>
+      </div>
     </nav>
   );
 };
