cdb7064815565f47c57a9967d023d8c44bc6333e
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-06-22 13:39:20 +0545

    Explor nav

--- Sanskriti/src/Components/Pages/Explore.jsx
+++ Sanskriti/src/Components/Pages/Explore.jsx
@@ -1,28 +1,59 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import dokoImg from '../../assets/doko.png';
+import daloImg from '../../assets/dalo.png';
+import pathiImg from '../../assets/pathi.png';
+import jatoImg from '../../assets/jato.png';
+import haloImg from '../../assets/halo.png';
+import dhikiImg from '../../assets/dhiki.png';
+import Flipcard from './flipcard';
+const product =[
+  {
+    title:'Doko (डोको)',
+    image:dokoImg,
+    description:'Hand woven cone-shaped bamboo basket.',
+    useText:'Historically, the doko was used by Nepalese villagers to carry crops, goods, and firewood.',
+    },
+      {
+    title:'Dalo (डालो)',
+    image:daloImg,
+    description:'A round, shallow bamboo basket with a flat base.',
+    useText:' Commonly used to carry grains, vegetables, or fruits—especially during harvests or market visits.',
+    },
+      {
+    title:'Pathi (पाथी)',
+    image:pathiImg,
+    description:' Measuring container made of brass, wood, or bamboo.',
+    useText:'Used to measure grains like rice, maize, and wheat. One Pathi is equal to around 4.5 kg of rice..',
+    },
+      {
+    title:'Jato (जाँतो)',
+    image:jatoImg,
+    description:'Stone grinder made from two circular stone.',
+    useText:'Used to grind grains like corn, millet, and wheat into flour manually by rotating the top stone.',
+    },
+      {
+    title:'Halo (हलो)',
+    image:haloImg,
+    description:'A wooden plough, often attached to oxen.',
+    useText:'Historically, the doko was used by Nepalese villagers to carry crops, goods, and firewood.',
+    },
+    {
+    title:'Dhiki (ढिकी)',
+    image:dhikiImg,
+    description:'Wooden lever system used for pounding grains.',
+    useText:'Used in traditional Nepali agriculture to till and prepare the soil before planting.',
+    },
+  ]
 const Explore = () => {
      const navigate=useNavigate();
   return (
-    <div className= 'bg-gradient-to-b from-[#B07A5A] to-[#FFD89A] bg-cover bg-center bg-no-repeat min-h-[calc(100vh)] flex flex-col justify-center items-center'>
-<div className="w-72 h-96 perspective">
-  <div className="flip-card-inner">
-    <div className="flip-card-front bg-white/55 p-6 flex flex-col items-center justify-center">
-      <img className="h-48" src={dokoImg} alt="Doko" />
-      <h1 className="text-3xl text-gray-800 mt-4 font-bold">Doko</h1>
-      <p className="text-xl text-gray-800 mt-2">Hand woven bamboo basket.</p>
+    <div className='bg-gradient-to-b from-[#B07A5A] to-[#FFD89A] min-h-screen flex flex-wrap justify-center gap-10 p-10'>
+      {product.map((items,index)=>(
+        <Flipcard key={index} title={items.title} image={items.image} description={items.description} useText={items.useText}/>
+      ))}
     </div>
-    <div className="flip-card-back p-6 flex flex-col items-center justify-center">
-      <h2 className="text-2xl text-gray-800 font-semibold mb-2">Use</h2>
-      <p className="text-lg text-gray-700 text-center">
-        Historically, the doko was used by Nepalese villagers to carry crops, goods, and firewood.
-      </p>
-      <button onClick={()=> navigate('/shop')} className='text-white w-20 h-10 bg-black rounded-3xl'>Shop</button>
-    </div>
-  </div>
-</div>
-    </div>
-  )
-}
+    );
+};
 
 export default Explore
--- Sanskriti/src/Components/Pages/flipcard.jsx
+++ Sanskriti/src/Components/Pages/flipcard.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-
-const Orders = () => {
+import React from 'react';
+import { useNavigate } from 'react-router-dom'
+import '../../index.css'; 
+const Flipcard = ({title,image,description,useText}
+) => {
+  const navigate=useNavigate();
   return (
-    <div>
+    <div className='w-72 h-86 mt-10 perspective'>
+      <div className='flip-card-inner'>
+        <div className='flip-card-front bg-white/55 p-6 flex flex-col items-center justify-center '>
+<img className="h-48 w-50 rounded-2xl" src={image} alt={title} />
+<h1 className='text-3xl text-gray-800 mt-4 font-bold'>{title} </h1>
+<p className='text-1xl text-gray-800 mt-2'>{description}</p>
+        </div>
+        <div className='flip-card-back p-6 flex flex-col items-center justify-center'>
+          <h2 className='text-2xl text-gray-800 font-semibold mb-2'>Use</h2>
+          <p className='text-lg text-gray-700 text-center'>{useText}</p>
+ <button onClick={() => navigate('/shop')} className='text-white w-20 h-10 bg-black rounded-3xl mt-4'>Shop</button>
+        </div>
+      </div>
       
     </div>
   )
 }
 
-export default Orders
+export default Flipcard
