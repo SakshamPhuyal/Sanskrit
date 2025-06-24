6ddd14a26933c092630d855d8aa90b2fbabb4dfc
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-06-24 21:49:57 +0545

    Contact bg

--- Sanskriti/src/Components/Pages/Contact.jsx
+++ Sanskriti/src/Components/Pages/Contact.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      
+    <div className='min-h-screen bg-[#d6b98c] p-6 flex'>
+      <p className='justify-center mt-10 text-3xl'>this site</p>
     </div>
   )
 }
