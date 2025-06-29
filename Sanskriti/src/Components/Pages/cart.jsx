224e6e5e71c0d6c1b65f8a684973320829e262ee
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-06-29 21:23:55 +0545

    cart done

--- Sanskriti/src/Components/Pages/cart.jsx
+++ Sanskriti/src/Components/Pages/cart.jsx
@@ -1,4 +1,4 @@
-
+//send to login after button
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
@@ -38,6 +38,11 @@ const Cart = ({ items: initialItems }) => {
     setItems(updatedItems);
     setQuantities(updatedQuantities);
   };
+    const totalPrice = items.reduce((total, item, index) => {
+    const price = parseInt(item.price.replace(/[^\d]/g, '')) || 0;
+    const quantity = quantities[index] || 1;
+    return total + price * quantity;
+  }, 0);
   return (
     <div>
       <div className="min-h-screen bg-gradient-to-b from-[#B07A5A] to-[#FFD89A] p-10 gap-10 flex justify-center">
@@ -53,9 +58,9 @@ const Cart = ({ items: initialItems }) => {
             >
               <p className='text-2xl text-amber-950'>{item.title}</p>
               <div className='flex items-center gap-2'>
-                <button onClick={() => increaseQty(index)} className='bg-black text-white w-10 h-10 rounded-full'>+</button>
+                <button onClick={() => increaseQty(index)} className='bg-black flex justify-center item-center text-white w-10 h-10 rounded-full text-2xl size-fit'>+</button>
                 <p className='text-2xl'>Qty:{quantities[index]}</p>
-                <button onClick={() => decreaseQty(index)}  className='bg-black text-white w-10 h-10 rounded-full'>-</button>
+                <button onClick={() => decreaseQty(index)}  className='bg-black flex justify-center item-center text-white w-10 h-10 rounded-full text-2xl size-fit'>-</button>
               </div>
               <p className='text-2xl'>
   Price: Rs. {parseInt(item.price.replace(/[^\d]/g, '')) * quantities[index]}
@@ -63,8 +68,16 @@ const Cart = ({ items: initialItems }) => {
 <button onClick={() => handleRemove(index)} className="cursor-pointer text-red-600 text-2xl">
   <FontAwesomeIcon icon={faTrash} />
 </button>
+
             </div>
           ))}
+          <div className='mt-4 text-amber-950 flex felx-col justify-between'>
+                    <p className='text-3xl'>Total Price:</p>
+                    <p className='text-3xl mr-2'>Rs. {totalPrice}</p>
+          </div>
+          <div className='flex justify-center mt-10'>
+          <button className='bg-black text-white w-40 h-10 rounded-2xl justify-center  cursor-pointer hover:opacity-85 text-2xl'> Order now</button>
+          </div>
         </div>
       </div>
     </div>
