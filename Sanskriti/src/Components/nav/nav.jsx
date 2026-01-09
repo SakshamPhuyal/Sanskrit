3fe99abc29acb9593db0d0c5cf684ee649b01bce
SakshamPhuyal <phuyalsaksham1@gmail.com>
2026-01-09 20:39:47 +0545

    logo changed

--- Sanskriti/src/Components/nav/nav.jsx
+++ Sanskriti/src/Components/nav/nav.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import logo from "../../assets/sblogo.png";
 const Nav = () => {
   return (
     <nav className="bg-[#5a3e36]/70 text-white shadow-md p-4 flex h-16 justify-between px-4 md:px-20 fixed top-0 w-full z-50">
-      <div className="flex gap-1 md:gap-3 text-xs md:text-3xl items-center">
+      <div className="flex gap-1 md:gap-1 text-xs md:text-3xl items-center">
         <p>Sanskriti Bazar</p>
         <img
-          className="h-4 w-4 md:h-9 md:w-9 rounded-lg"
-          src="https://imgs.search.brave.com/7OKwT_y5F8aVe2VImisGFjaW_ThkDFcKGpodUJk0jec/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/ZHJpYmJibGUuY29t/L3VzZXJzLzEyNDQ0/Ny9zY3JlZW5zaG90/cy8xNzkyMzU2L3Nh/bnNrcml0aS5qcGc_/cmVzaXplPTQwMHgz/MDAmdmVydGljYWw9/Y2VudGVy"
+          className="h-4 w-4 md:h-20 mt-2 md:w-20 ml-0 rounded-lg"
+          src={logo}//https://imgs.search.brave.com/7OKwT_y5F8aVe2VImisGFjaW_ThkDFcKGpodUJk0jec/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/ZHJpYmJibGUuY29t/L3VzZXJzLzEyNDQ0/Ny9zY3JlZW5zaG90/cy8xNzkyMzU2L3Nh/bnNrcml0aS5qcGc_/cmVzaXplPTQwMHgz/MDAmdmVydGljYWw9/Y2VudGVy
           alt=""
         />
       </div>
